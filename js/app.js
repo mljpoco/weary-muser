77f5191cd5a89b94766a67dbe0011d4f6f84d1c5
Scribeguru <pocomitch@gmail.com>
2021-10-17 13:25:19 -0700

    sharing buttons fully configured, dynamic og:url content and styling changes

--- js/app.js
+++ js/app.js
@@ -16,8 +16,9 @@ function renderArticle() {
     body.innerHTML = articles.find(
     article => url+article.href === window.location.href).body;
   }
-  loadTitleForShare(title);
+  loadTitleAndUrlForShare(title);
   siftSearch(title);
+  shareButtons();
 }
 
 function siftSearch(title) {
@@ -33,8 +34,39 @@ function siftSearch(title) {
   older.removeAttribute('href');
 }
 
-function loadTitleForShare(title) {
+function loadTitleAndUrlForShare(title) {
   document.getElementById('ogTitle').setAttribute('content', title.innerHTML);
+  document.getElementById('ogUrl').setAttribute('content', window.location.href);
+}
+
+// Most social media sharing is as simple as adding your link to the end of
+// their sharing path. Snapchat requires you to use their SDK.
+function shareButtons() {
+  const twitter = document.getElementById('twitter');
+  const facebook = document.getElementById('facebook');
+  const reddit = document.getElementById('reddit');
+  const tumblr = document.getElementById('tumblr');
+  const copyLink = document.getElementById('copyLink');
+
+  twitter.addEventListener('click', () => {
+    twitter.target = '_blank';
+    twitter.href = 'https://twitter.com/intent/tweet?url=' + window.location.href;
+  });
+  facebook.addEventListener('click', () => {
+    facebook.target = '_blank';
+    facebook.href = 'https://www.facebook.com/sharer/sharer.php?u=' + window.location.href;
+  })
+  reddit.addEventListener('click', () => {
+    reddit.target = '_blank';
+    reddit.href = 'https://www.reddit.com/submit?url=' + window.location.href;
+  });
+  tumblr.addEventListener('click', () => {
+    tumblr.target = '_blank';
+    tumblr.href = 'https://www.tumblr.com/widgets/share/tool?shareSource=legacy&canonicalUrl=' + 'window.location.href';
+  })
+  copyLink.addEventListener('click', () => {
+    navigator.clipboard.writeText(window.location.href);
+  });
 }
 
 renderArticle();
