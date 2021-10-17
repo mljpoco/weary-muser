0a3b9c61d969660356aef4aa7dedc58fa5f1a54e
Scribeguru <pocomitch@gmail.com>
2021-10-16 20:01:56 -0700

    Article navigation  methods complete, snapchat and twitter share buttons implemented

--- js/app.js
+++ js/app.js
@@ -1,28 +1,40 @@
 'use strict';
-import {articles} from './articles.js';
 
-const newer = document.getElementById('siftNewer');
-const older = document.getElementById('siftOlder');
+import { articles } from './articles.js';
 
-let articleViewer = {
-  aIndex: 0,
-  siftNewer() {
-    articleViewer.aIndex = (articleViewer.aIndex > 0) ? --articleViewer.aIndex : articleViewer.aIndex;
-    console.log('siftNewer fires', 'aIndex: ' + articleViewer.aIndex);
-    renderArticle();
-  },
-  siftOlder() {
-    articleViewer.aIndex = (articleViewer.aIndex < articles.length - 1) ? ++articleViewer.aIndex : articleViewer.aIndex;
-    console.log('siftOlder fires', 'aIndex: ' + articleViewer.aIndex);
-    renderArticle();
-  },
-};
+let url = 'http://localhost:3000/';
 
 function renderArticle() {
-  newer.addEventListener('click', articleViewer.siftNewer);
-  older.addEventListener('click', articleViewer.siftOlder);
-  document.getElementById('titleWrapper').innerHTML = articles[articleViewer.aIndex].title;
-  document.getElementById('bodyWrapper').innerHTML = articles[articleViewer.aIndex].body;
+  const title = document.getElementById('titleWrapper');
+  const body = document.getElementById('bodyWrapper');
+  if (window.location.href === 'http://localhost:3000/index.html' || window.location.href === url) {
+    title.innerHTML = articles[0].title;
+    body.innerHTML = articles[0].body;
+  } else {
+    title.innerHTML = articles.find(
+    article => url+article.href === window.location.href).title;
+    body.innerHTML = articles.find(
+    article => url+article.href === window.location.href).body;
+  }
+  loadTitleForShare(title);
+  siftSearch(title);
+}
+
+function siftSearch(title) {
+  const newer = document.getElementById('siftNewer');
+  const older = document.getElementById('siftOlder');
+  let currentPos = articles.findIndex(article => article.title === title.innerHTML);
+  console.log('entry number '+articles[currentPos].id);
+  (currentPos > 0) ?
+  newer.href = articles[currentPos - 1].href :
+  newer.removeAttribute('href');
+  (currentPos < articles.length - 1) ?
+  older.href = articles[currentPos + 1].href :
+  older.removeAttribute('href');
+}
+
+function loadTitleForShare(title) {
+  document.getElementById('ogTitle').setAttribute('content', title.innerHTML);
 }
 
 renderArticle();
---
+++ js/snap.js
@@ -0,0 +1,30 @@
+
+// Load the SDK asynchronously
+
+(function (d, s, id) {
+
+  var js,
+
+    sjs = d.getElementsByTagName(s)[0];
+
+  if (d.getElementById(id)) return;
+
+  js = d.createElement(s);
+
+  js.id = id;
+
+  js.src = "https://sdk.snapkit.com/js/v1/create.js";
+
+  sjs.parentNode.insertBefore(js, sjs);
+
+})(document, "script", "snapkit-creative-kit-sdk");
+
+window.snapKitInit = function () {
+
+  snap.creativekit.initalizeShareButtons(
+
+    document.getElementsByClassName("snapchat-share-button")
+
+  );
+
+};
